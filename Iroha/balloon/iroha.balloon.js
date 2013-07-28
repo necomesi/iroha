cc7e016adc999f859dce1ff35a4af7cc9ea17100
kojika <kojika@necomesi.jp>
2013-07-28 16:44:51 +0900

    Iroha.Balloon : バルーン非表示時の resizeTo() で表示位置を再調整をさせない。
    非表示時に位置際調整してしまうと、画面外に放り出していたはずのバルーンが画面内の所定位置に戻ってきてしまう。

--- Iroha/balloon/iroha.balloon.js
+++ Iroha/balloon/iroha.balloon.js
@@ -3,7 +3,7 @@
  *    @fileoverview
  *       Balloon (Floating Layer) Controls.
  *
- *    @version 3.03.20130108
+ *    @version 3.03.20130728
  *    @requires jquery.js
  *    @requires iroha.js
  *    @requires iroha.balloon.css
@@ -23,57 +23,57 @@ Iroha.Balloon = function() {
 	/** base node of the balloon
 	    @type jQuery */
 	this.$node = $();
-	
+
 	/** content body node in the balloon.
 	    @type jQuery
 	    @private */
 	this.$body = $();
-	
+
 	/** flag of activity of the balloon
 	    @type Boolean
 	    @private */
 	this.active = false;
-	
+
 	/** flag of ballon's position is fixed or not.
 	    @type Boolean
 	    @private */
 	this.isPosFixed = false;
-	
+
 	/** X-coordinate of the balloon (px)
 	    @type Number
 	    @private */
 	this.posX = 0;
-	
+
 	/** Y-coordinate of the balloon (px)
 	    @type Number
 	    @private */
 	this.posY = 0;
-	
+
 	/** X-distance from original balloon position (px)
 	    @type Number
 	    @private */
 	this.offsetX = 0;
-	
+
 	/** Y-distance from original balloon position (px)
 	    @type Number
 	    @private */
 	this.offsetY = 0;
-	
+
 	/** use position revising on X-axis?
 	    @type Boolean
 	    @private */
 	this.posReviseX = false;
-	
+
 	/** use position revising on Y-axis?
 	    @type Boolean
 	    @private */
 	this.posReviseY = false;
-	
+
 	/** ignore X-axis when {@link #moveToCenter} is called?
 	    @type Boolean
 	    @private */
 	this.ignoreX = false;
-	
+
 	/** ignore Y-axis when {@link #moveToCenter} is called?
 	    @type Boolean
 	    @private */
@@ -91,7 +91,7 @@ $.extend(Iroha.Balloon,
 	 * @constant
 	 */
 	BASE_CLASSNAME : 'iroha-balloon',
-	
+
 	/**
 	 * 新しくインスタンスを生成するか、基底要素ノードから既存のインスタンスを得る。
 	 * 基底要素ノードは init() で自動的に作られる。
@@ -113,11 +113,11 @@ $.extend(Iroha.Balloon.prototype,
 	 */
 	init : function(setting) {
 		var setting = $.extend(Iroha.Balloon.Setting.create(), setting);
-		
+
 		this.$node = $(document.createElement('ins'));
 		this.$body = this.$node;
 		this.hide();
-		
+
 		this.addClass     (this.constructor.BASE_CLASSNAME       );
 		this.addClass     (setting.className                     );
 		this.setId        (setting.id                            );
@@ -128,19 +128,19 @@ $.extend(Iroha.Balloon.prototype,
 		this.setPosRevise (setting.posReviseX, setting.posReviseY);
 		this.moveTo       (setting.posX      , setting.posY      );
 		this.moveToCenter (setting.ignoreX   , setting.ignoreY   );
-	
+
 		return this;
 	},
-	
+
 	/**
 	 * このインスタンスを破棄する
 	 */
 	dispose : function() {
 		this.$node && this.$node.remove();
-		
+
 		this.constructor.disposeInstance(this);
 	},
-	
+
 	/**
 	 * get activity state of the balloon.
 	 * @return true if the balloon is active (visible)
@@ -149,7 +149,7 @@ $.extend(Iroha.Balloon.prototype,
 	isActive : function() {
 		return this.active;
 	},
-	
+
 	/**
 	 * append the balloon base node to the specified element node.
 	 * @param {jQuery|Element|String} target    expression of target element to append
@@ -164,7 +164,7 @@ $.extend(Iroha.Balloon.prototype,
 		}
 		return this;
 	},
-	
+
 	/**
 	 * add specified class name to the base node of the balloon.
 	 * @param {String} className     class name to add
@@ -177,7 +177,7 @@ $.extend(Iroha.Balloon.prototype,
 		}
 		return this;
 	},
-	
+
 	/**
 	 * remove specified class name to the base node of the balloon.
 	 * @param {String} className     class name to add
@@ -190,7 +190,7 @@ $.extend(Iroha.Balloon.prototype,
 		}
 		return this;
 	},
-	
+
 	/**
 	 * set fragment-id to the base node of the balloon.
 	 * @param {String} id    id-string to add
@@ -203,7 +203,7 @@ $.extend(Iroha.Balloon.prototype,
 		}
 		return this;
 	},
-	
+
 	/**
 	 * set offset value (distance from original balloon position).
 	 * @param {Number} [x]    offset on X-axis (px)
@@ -217,7 +217,7 @@ $.extend(Iroha.Balloon.prototype,
 		this.moveTo(); // revise position if if necessary.
 		return this;
 	},
-	
+
 	/**
 	 * set position revisiong setting.
 	 * @param {Boolean} [x]    if true, it uses X-axis when position revising
@@ -231,7 +231,7 @@ $.extend(Iroha.Balloon.prototype,
 		this.moveTo(); // revise position if if necessary.
 		return this;
 	},
-	
+
 	/**
 	 * make the balloon's position to be fixed, and provides the way to ignore position-fix at X-scrolling and Y-scrolling.
 	 * with feature of ignoring position-fix at X or Y axis, and hiding block during scrolling.
@@ -243,16 +243,16 @@ $.extend(Iroha.Balloon.prototype,
 	 */
 	setPositionFixed : function(ignoreX, ignoreY, autoHide) {
 		this.isPosFixed = true;
-		
+
 		//
 		//
 		// あとで何か書く
 		//
 		//
-		
+
 		return this;
 	},
-	
+
 	/**
 	 * apply template to the balloon.
 	 * @param {String} [template]    a template represented by HTML-string; if nonspecified, the template structure is removed from the balloon.
@@ -262,7 +262,7 @@ $.extend(Iroha.Balloon.prototype,
 	 */
 	applyTemplate : function(template, bodyExpr) {
 		var $content = this.getContent();
-	
+
 		if (!template) {
 			this.$body = this.$node.empty().append($content);
 		} else if (!bodyExpr) {
@@ -272,10 +272,10 @@ $.extend(Iroha.Balloon.prototype,
 		} else {
 			this.$body = this.$node.empty().append(template).find(bodyExpr).append($content);
 		}
-		
+
 		return this;
 	},
-	
+
 	/**
 	 * get content of the balloon.
 	 * @return jQuery object which contains element nodes in this balloon.
@@ -284,7 +284,7 @@ $.extend(Iroha.Balloon.prototype,
 	getContent : function() {
 		return this.$body.contents();
 	},
-	
+
 	/**
 	 * set content of the balloon.
 	 * @param {NodeList|Element|Element[]|jQuery|String} [content]    content to add to balloon
@@ -294,7 +294,7 @@ $.extend(Iroha.Balloon.prototype,
 	setContent : function(content) {
 		return this.clearContent(true).addContent(content);
 	},
-	
+
 	/**
 	 * add content to the balloon.
 	 * @param {NodeList|Element|Element[]|jQuery|String} [content]    content to add to balloon
@@ -305,16 +305,16 @@ $.extend(Iroha.Balloon.prototype,
 		var before = this.getGeometry();
 		this.$body.append(content);
 		var after  = this.getGeometry();
-	
+
 		if (before.width != after.width || before.height != after.height) {
 			this.doCallbackByName('onResize');
 			this.moveTo(); // revise position if necessary.
 		}
-	
+
 		this.doCallbackByName('onContentChange');
 		return this;
 	},
-	
+
 	/**
 	 * remove all contents from the balloon.
 	 * @param {Boolean} [noCallback]    if true, it doesn't callback.
@@ -324,13 +324,13 @@ $.extend(Iroha.Balloon.prototype,
 	clearContent : function(noCallback) {
 		var hadContent = this.hasContent();
 		this.$body.empty();
-	
+
 		if (!noCallback && hadContent) {
 			this.doCallbackByName('onContentChange');
 		}
 		return this;
 	},
-	
+
 	/**
 	 * does the balloon the content (in the balloon body)?
 	 * @return true if the balloon has the content (in the balloon body).
@@ -339,7 +339,7 @@ $.extend(Iroha.Balloon.prototype,
 	hasContent : function() {
 		return (this.getContent().length > 0);
 	},
-	
+
 	/**
 	 * get balloon geometry
 	 * @return Iroha.Balloon.Geometry object.
@@ -355,7 +355,7 @@ $.extend(Iroha.Balloon.prototype,
 			, height  : this.$node.outerHeight()
 		};
 	},
-	
+
 	/**
 	 * show the balloon at specific coordinate.
 	 * @param {Number} [x]    X-coordinate to popup (px) (posX prop value is used in the case of no appointment)
@@ -372,7 +372,7 @@ $.extend(Iroha.Balloon.prototype,
 		}
 		return this;
 	},
-	
+
 	/**
 	 * resize the balloon.
 	 * @param {Number} [width=-1]     new width  (border-box width ) of the balloon, 0 means 'auto', -1 means 'no change'
@@ -385,20 +385,21 @@ $.extend(Iroha.Balloon.prototype,
 		if (typeof height != 'number') height = -1;
 		if (width  >= 0) this.$node.css('width' , width  == 0 ? 'auto' : width );
 		if (height >= 0) this.$node.css('height', height == 0 ? 'auto' : height);
-	
+
 		var offsetWidth  = this.$node.outerWidth();
 		var offsetHeight = this.$node.outerHeight();
 		var reviseWidth  = (width  >= 0) ? 2 * width  - offsetWidth  : -1;
 		var reviseHeight = (height >= 0) ? 2 * height - offsetHeight : -1;
-	
+
 		if (offsetWidth  != width  && reviseWidth  >= 0) this.$node.css('width' , reviseWidth );
 		if (offsetHeight != height && reviseHeight >= 0) this.$node.css('height', reviseHeight);
-	
+
 		this.doCallbackByName('onResize');
-		this.moveTo(); // revise position if if necessary.
+		this.isActive() && this.moveTo(); // revise position if if necessary.
+
 		return this;
 	},
-	
+
 	/**
 	 * move balloon a specified number of pixels relative to its current coordinates
 	 * this method does not make the balloon shown.
@@ -413,7 +414,7 @@ $.extend(Iroha.Balloon.prototype,
 		this.moveTo(this.posX + x, this.posY + y);
 		return this;
 	},
-	
+
 	/**
 	 * move balloon to specified coordinates.
 	 * this method does not make the balloon shown.
@@ -427,18 +428,18 @@ $.extend(Iroha.Balloon.prototype,
 		var oldY = this.posY;
 		if (typeof x == 'number') this.posX = x;
 		if (typeof y == 'number') this.posY = y;
-	
+
 		this.revisePosition();
-	
+
 		this.$node.css('left', this.posX + this.offsetX);
 		this.$node.css('top' , this.posY + this.offsetY);
-	
+
 		if (oldX != this.posX || oldY != this.posY) {
 			this.doCallbackByName('onMove');
 		}
 		return this;
 	},
-	
+
 	/**
 	 * move balloon to center of the browser window.
 	 * this method does not make the balloon shown.
@@ -456,19 +457,19 @@ $.extend(Iroha.Balloon.prototype,
 		} else {
 			geom = { parentW : $(base).outerWidth(), parentH : $(base).outerHeight() };
 		}
-	
+
 		if (typeof ignoreX == 'boolean') this.ignoreX = ignoreX;
 		if (typeof ignoreY == 'boolean') this.ignoreY = ignoreY;
-	
+
 		var posX = (!this.ignoreX) ? (geom.parentW - this.$node.outerWidth ()) / 2 : this.posX; if (posX < 0) posX = 0;
 		var posY = (!this.ignoreY) ? (geom.parentH - this.$node.outerHeight()) / 2 : this.posY; if (posY < 0) posY = 0;
 		var scrX = (this.isPosFixed || !geom.scrollX) ? 0 : geom.scrollX;
 		var scrY = (this.isPosFixed || !geom.scrollY) ? 0 : geom.scrollY;
-	
+
 		this.moveTo(posX + scrX, posY + scrY);
 		return this;
 	},
-	
+
 	/**
 	 * reviseing the balloon position to be in the window frame.
 	 * @private
@@ -483,23 +484,23 @@ $.extend(Iroha.Balloon.prototype,
 			var Safari2    = (Iroha.ua.isSafari && Iroha.ua.version <  522);
 			var edgeRight  = (IE7 || Safari2 || MacIE || geom.pageH <= geom.windowH) ? 0 : geom.scrollBar;
 			var edgeBottom = (IE7 || Safari2 || MacIE || geom.pageW <= geom.windowW) ? 0 : geom.scrollBar;
-	
+
 			if (IE6 || IE8) {
 				edgeRight   = geom.scrollBar + 4;
 				edgeBottom += 4;
 			}
-	
+
 			var offset  = this.$node.offsetParent().offset();
 			var reviseX = (geom.windowW + geom.scrollX) - (offset.left + this.posX + this.offsetX + this.$node.outerWidth()  + edgeRight );
 			var reviseY = (geom.windowH + geom.scrollY) - (offset.top  + this.posY + this.offsetY + this.$node.outerHeight() + edgeBottom);
 			var posX    = (this.posX < geom.scrollX) ? geom.scrollX : this.posX + (reviseX < 0 ? reviseX : 0);
 			var posY    = (this.posY < geom.scrollY) ? geom.scrollY : this.posY + (reviseY < 0 ? reviseY : 0);
-	
+
 			if (this.posReviseX) this.posX = posX;
 			if (this.posReviseY) this.posY = posY;
 		}
 	},
-	
+
 	/**
 	 * make the balloon hidden.
 	 * @return Iroha.Balloon instance
@@ -514,7 +515,7 @@ $.extend(Iroha.Balloon.prototype,
 		}
 		return this;
 	},
-	
+
 	/**
 	 * 表示・非表示を切り替え
 	 * @param {Boolean} [showOrHide]    真偽値を与えることで、表示・非表示を明示的に選択する。
@@ -527,10 +528,10 @@ $.extend(Iroha.Balloon.prototype,
 		           	: Boolean(showOrHide);
 		if (bool) this.show()
 		     else this.hide();
-		
+
 		return this;
 	},
-	
+
 	/**
 	 * z-index 値を取得する、または設定する。
 	 * @param {Number|String} [zIndex]    整数値、または "auto" を指定。無指定時は z-index 値を取得する getter として動作。
@@ -545,7 +546,7 @@ $.extend(Iroha.Balloon.prototype,
 			return this;
 		}
 	},
-	
+
 	/**
 	 * process callback.
 	 * @param {String} name    callback name (preferred to start with 'on')
@@ -568,80 +569,80 @@ Iroha.Balloon.Setting = function() {
 	 * @type NodeList|Element|Element[]|jQuery|String
 	 */
 	this.content = '';
-	
+
 	/**
 	 * an element where the balloon will be appended to.
 	 * @type jQuery|Element|String
 	 */
 	this.appendTo = '';
-	
+
 	/**
 	 * additional className of base node of the balloon
 	 * @type String
 	 */
 	this.className = '';
-	
+
 	/**
 	 * fragment-id of base node of the balloon
 	 * @type String
 	 */
 	this.id = '';
-	
+
 	/**
 	 * a template represented by HTML-string
 	 * @type String
 	 */
 	this.template = '';
-	
+
 	/**
 	 * an expression to find content body element in the teplate;
 	 * this is required when the 'template' property is given.
 	 * @type String
 	 */
 	this.bodyExpr = '';
-	
+
 	/**
 	 * initial X-coordinate of the balloon (px)
 	 * @type Number
 	 */
 	this.posX = 0;
-	
+
 	/**
 	 * initial Y-coordinate of the balloon (px)
 	 * @type Number
 	 */
 	this.posY = 0;
-	
+
 	/**
 	 * X-distance from original balloon position (px)
 	 * @type Number
 	 */
 	this.offsetX = 0;
-	
+
 	/**
 	 * Y-distance from original balloon position (px)
 	 * @type Number
 	 */
 	this.offsetY = 0;
-	
+
 	/**
 	 * use position revising on X-axis?
 	 * @type Boolean
 	 */
 	this.posReviseX = false;
-	
+
 	/**
 	 * use position revising on Y-axis?
 	 * @type Boolean
 	 */
 	this.posReviseY = false;
-	
+
 	/**
 	 * ignore centering on X-axis?
 	 * @type Boolean
 	 */
 	this.ignoreX = false;
-	
+
 	/**
 	 * ignore centering on Y-axis?
 	 * @type Boolean
@@ -669,31 +670,31 @@ Iroha.Balloon.Geometry = function() {
 	 * @type Number
 	 */
 	this.posX = 0;
-	
+
 	/**
 	 * Y-coordinate of the balloon (px)
 	 * @type Number
 	 */
 	this.posY = 0;
-	
+
 	/**
 	 * X-distance from original balloon position (px)
 	 * @type Number
 	 */
 	this.offsetX = 0;
-	
+
 	/**
 	 * Y-distance from original balloon position (px)
 	 * @type Number
 	 */
 	this.offsetY = 0;
-	
+
 	/**
 	 * width (border-box width) of the balloon (px)
 	 * @type Number
 	 */
 	this.width = 0;
-	
+
 	/**
 	 * height (border-box height) of the balloon (px)
 	 * @type Number
