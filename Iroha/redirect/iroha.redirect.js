ff263fc32dee425dde5606d63758c39307d98006
kojika <kojika@necomesi.jp>
2013-09-29 21:37:03 +0900

    iroha.redirect.js : IE8 以前の IE がうんこすぎて、スタイルルールの動的追加・削除処理にスクリプトエラーが出るようになっていた。

--- Iroha/redirect/iroha.redirect.js
+++ Iroha/redirect/iroha.redirect.js
@@ -1,34 +1,35 @@
+/*! "iroha.redirect.js" | Iroha - Necomesi JS Library | by Necomesi Ltd. */
 /* -------------------------------------------------------------------------- */
 /**
  *    @fileoverview
  *       url redirector
  *
- *    @version 3.00.20130316
+ *    @version 3.01.20130927
  *
  *    @example
  *    <script src="iroha.redirect.js?url=URL;delay=3;hide=yes"></script>
  *      - url    : absolute/relative URL string (requred)
- *      - hide   : if "yes" then whole page will be hidden. (default:no) 
- *      - delay  : number of seconds to delay redirecttion. (default:0) 
+ *      - hide   : if "yes" then whole page will be hidden. (default:no)
+ *      - delay  : number of seconds to delay redirecttion. (default:0)
  *      - appear : number of seconds to before page appears again. (default:0) (0 means "never appear")
  */
 /* -------------------------------------------------------------------------- */
 
 (function() {
 	var THISFILE = /^iroha\.redirect([\.\-].+)?\.js$/;  // matches this file's filename.
-	
-	
-	
+
+
+
 	var head    = document.getElementsByTagName('head')[0];
 	var scripts = head ? head.getElementsByTagName('script') : [];
-	
+
 	// find script element by filename...
 	for (var i = 0, n = scripts.length; i < n; i++) {
 		var script = scripts[i];
 		var fname  = script.src.split('?')[0].split('/').pop();
 		var query  = script.src.split('?').slice(1).join('?');
 		var param  = {};
-		
+
 		// found.
 		if (THISFILE.test(fname)) {
 			var pairs = (query || '').split(';');
@@ -36,34 +37,40 @@
 				var pair = pairs[j].split('=');
 				param[pair[0]] = pair.slice(1).join('=') || '';
 			};
-			
+
 			// "url" param value is required.
 			if (param.url) {
 				var style;
-				
+
 				// add style rule to hide whole page.
 				if (param.hide == 'yes') {
-					style           = document.createElement('style');
-					style.innerHTML = 'html, body { display: none !important }';
-					style.id        = 'iroha-redirector-injected';
+					style    = document.createElement('style');
+					style.id = 'iroha-redirector-injected';
 					head.insertBefore(style, script.nextSibling);
+					var sheets = document.styleSheets;
+					var sheet  = sheets[sheets.length - 1];
+					if (sheet) {
+						sheet.insertRule
+							? sheet.insertRule('html { display: none !important }', 0)             // modern Browsers
+							: sheet.addRule && sheet.addRule('html', 'display: none !important');  // old Fxxing IEs
+					}
 				}
-				
+
 				param.delay  = Math.max(0, param.delay ) || 0;
 				param.appear = Math.max(0, param.appear) || 0;
-				
+
 				// do redirect after delay.
 				setTimeout(function() {
 					// show page again after delay.
 					param.appear > 0 && setTimeout(function() {
-						style && style.parentNode.removeChild(style);
+						style && style.parentNode && style.parentNode.removeChild(style);
 					}, param.appear * 1000);
-					
+
 					// redirection
 					location.replace(param.url);
 				}, param.delay * 1000);
 			}
-			
+
 			break;
 		}
 	}
